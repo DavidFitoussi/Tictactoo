36ea1ba0c9f1c7f7091c129abc4e3a246515b166
DavidFitoussi <52169384+DavidFitoussi@users.noreply.github.com>
2019-07-21 22:10:53 +0300

    test of diagonal

--- Game.py
+++ Game.py
@@ -4,7 +4,8 @@ from enum import Enum
 class Direction(Enum):
     Row = 1
     Column = 2
-    Diagonal = 3
+    DiagonalX = 3
+    DiagonalY = 4
 
 class clsGame:
     def __init__(self):
@@ -30,18 +31,24 @@ class clsGame:
         else: return False
 
     def IsWinner(self):
-        return (self.DirectionCheck(Direction.Row) or
-                  self.DirectionCheck(Direction.Column) or
-                  self.DirectionCheck(Direction.Diagonal)
+        return (#self.DirectionCheck(Direction.Row) or
+                #self.DirectionCheck(Direction.Column) or
+                #self.DirectionCheck(Direction.DiagonalX) or
+                self.DirectionCheck(Direction.DiagonalY)
                   )
 
     def DirectionCheck(self,direction):
+        Counter = 0
         for x in range(0, self.InstanceResource.MatrixSize):
-            Counter = 0
+            if (direction != Direction.DiagonalY) and (direction != Direction.DiagonalX):
+                Counter = 0
             for y in range(0, self.InstanceResource.MatrixSize):
-                if (((direction.Row) and (self.MatrixArray[x-1][y-1] == self.CurrentSign)) or
-                   # ((direction.Diagonal) and (self.MatrixArray[x - 1][x - 1] == self.CurrentSign)) or
-                    ((direction.Column) and (self.MatrixArray[y - 1][x - 1] == self.CurrentSign))):
+                if (
+                    ((direction == Direction.Row) and (self.MatrixArray[x-1][y-1] == self.CurrentSign)) or
+                    ((direction == Direction.DiagonalX) and (self.MatrixArray[x - 1][x - 1] == self.CurrentSign) and (x==y)) or
+                    ((direction == Direction.DiagonalY) and (self.MatrixArray[x - 1][x - 1] == self.CurrentSign) and (y == self.InstanceResource.MatrixSize -x)) or
+                    ((direction == Direction.Column) and (self.MatrixArray[y - 1][x - 1] == self.CurrentSign))
+                ):
                     Counter += 1
                     if (Counter == self.InstanceResource.MatrixSize):
                         return True
